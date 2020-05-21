04edc79fca1f14f69bf772f48281367fc63c6a79
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-05-21 20:21:36 +0300

    add cors-anywhere.herokuapp proxy

--- src/js/constants/api-config.js
+++ src/js/constants/api-config.js
@@ -1,6 +1,6 @@
 const MAIN_API_URL = 'https://api.sudomoin-pavel-news-explorer.tk';
 
-const NEWS_API_URL = 'https://newsapi.org';
+const NEWS_API_URL = 'https://cors-anywhere.herokuapp.com/newsapi.org';
 const NEWS_API_KEY = '8d5e1fca9e5445dc8b38248471bbe952';
 const NEWS_API_PERIOD = 7;
 
